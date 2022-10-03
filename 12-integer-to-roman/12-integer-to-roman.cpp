1558b27185b97608239685c61efe5794884853ab
TalaDweikat <106847094+TalaDweikat@users.noreply.github.com>
2022-10-03 22:11:31 +0300

    Time: 12 ms (63.14%), Space: 6.1 MB (50.39%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string v="";
+        int i=0;
+        while(num!=0){
+            if(num>=1000){
+                num-=1000;
+                v+="M";
+                continue;
+            }
+            if(num>=900){
+                num-=900;
+                v+="CM";
+                continue;
+            }
+            if(num>=500){
+                num-=500;
+                v+="D";
+                continue;
+            }
+            if(num>=400){
+                num-=400;
+                v+="CD";
+                continue;
+            }
+            if(num>=100){
+                num-=100;
+                v+="C";
+                continue;
+            }
+            if(num>=90){
+                num-=90;
+                v+="XC";
+                continue;
+            }
+            if(num>=50){
+                num-=50;
+                v+="L";
+                continue;
+            }
+            if(num>=40){
+                num-=40;
+                v+="XL";
+                continue;
+            }
+            if(num>=10){
+                num-=10;
+                v+="X";
+                continue;
+            }
+            if(num>=9){
+                num-=9;
+                v+="IX";
+                continue;
+            }
+            if(num>=5){
+                num-=5;
+                v+="V";
+                continue;
+            }
+            if(num>=4){
+                num-=4;
+                v+="IV";
+                continue;
+            }
+            if(num>=1){
+                num-=1;
+                v+="I";
+                continue;
+            }
+        }
+        return v;
+    }
+};
